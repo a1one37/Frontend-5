8d7c278648e76232fe19ccc1b1c64327d1d24e52
ilyamartynov989@gmail.com <ilyamartynov989@gmail.com>
2021-10-17 16:48:01 +0300

    4,5,6

---
+++ homework-1/index.js
@@ -0,0 +1,76 @@
+
+
+// ----------------------------------------------------------------------ЗАВДАННЯ 4-------------------------------------------------
+
+// const credits = 23580;
+// const pricePerDroid = 3000;
+// let totalPrice;
+// let ostatok;
+// let  droidsQuantity = prompt("How much droids u want?") ;
+
+// if(droidsQuantity === null){
+//     console.log("Скасовано користувачем!")
+// }
+// else{
+//     totalPrice= pricePerDroid * Number(droidsQuantity);           
+//  console.log(`Ваша сумма заказа ${totalPrice}`)
+// }
+// if(totalPrice>credits){
+//     console.log("Недостатньо коштів на рахунку!")
+// }
+// else{
+//     ostatok = credits - totalPrice;
+//     console.log(`Ви купили ${droidsQuantity} дроїда, на рахунку залишилося
+//       ${ostatok} кредитів.`)
+// }
+
+
+// ----------------------------------------------------------------------ЗАВДАННЯ 4-------------------------------------------------
+
+// ----------------------------------------------------------------------ЗАВДАННЯ 5-------------------------------------------------
+
+
+  //   let country = prompt("insert your country")
+    
+  
+
+
+  // switch(country){
+  //   case "China": 
+  //     alert ("Shipping to China will cost 100 credits");
+  //     break
+      
+  //   case "Chile":
+  //     alert("Shipping to Chile will cost 250 credits");
+  //     break
+      
+  //   case "Australia":
+  //       alert("Shipping to Australia will cost 170 credits");
+  //     break
+
+  //     case "India":
+  //       alert("Shipping to India will cost 80 credits") ;
+  //    break
+
+  //   case "Jamaica":
+  //       alert("Shipping to Jamaica will cost 120 credits") ;
+  //    break
+      
+  //     default :
+  //     alert("Sorry, there is no delivery to your country") ;
+  // }
+    
+// ----------------------------------------------------------------------ЗАВДАННЯ 5-------------------------------------------------
+// ----------------------------------------------------------------------ЗАВДАННЯ 6-------------------------------------------------
+    
+//     let total = 0;
+
+//     do{
+//       let input = prompt("Enter number")
+//       total += Number(input)  
+//     }while(input = null)
+  
+  
+// console.log(`Загальна сума чисел дорівнює ${total}`)
+
+// ----------------------------------------------------------------------ЗАВДАННЯ 6-------------------------------------------------
