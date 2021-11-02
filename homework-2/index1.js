092d06b53ae0764124bcf8df5fa79bbeaac36146
ilyamartynov989@gmail.com <ilyamartynov989@gmail.com>
2021-11-02 22:35:38 +0200

    homework 2

---
+++ homework-2/index1.js
@@ -0,0 +1,116 @@
+// ----------------------------------------------------------------- Задача 1 -----------------------------------------------------------------
+
+
+
+
+
+// let sum = 0;
+//      for (let i = 0; i <= 100; i += 3) {
+//          if(i % 2 === 0) {
+
+//             if(i >= 40 && i <=59){
+//                 continue;
+//                             }
+//          console.log(`Я цифра ${i}`)
+//          sum += i;
+
+             
+         
+//         }
+        
+//       }
+     
+//      console.log(`Сумма чисел - ${sum}`);
+
+
+
+
+// ----------------------------------------------------------------- Задача 2 --------------------------------------------------------------------------------
+
+// let input;
+// do{
+   
+//     input = prompt("Enter your age")
+   
+//     if(input === null){
+//         console.log("Скасовано користувачем");
+//     }
+    
+//   else if(input > 0 && input <= 2){
+// alert("you are a baby");
+//   }
+
+//   else if(input >=3 && input <= 18){
+//       alert("you are a child");
+//   }
+// else if(input >= 18 && input<=150){
+//     alert("you are an adult");
+// }
+
+// else if (input === ""){
+//     alert("you didn`t write anything");
+    
+// }
+// else if (input === " "){
+//     alert("it's just a space!!");
+    
+// }
+// else if(input >= 151)  {
+// alert(`it is fake age ${input}`)
+// }
+// else if(input <= 0)  {
+//     alert(`it is fake age ${input}`)
+//     }
+
+
+
+// let inputIsNaN = isNaN(Number(input))
+// if(inputIsNaN){
+//     alert("you entered not a number")
+
+
+
+// }
+
+// }while (input !== null)
+
+
+
+// ----------------------------------------------------------------- Задача 3 --------------------------------------------------------------------------------let input;
+
+// let input;
+// let sum = 0;
+// let total = 0;
+
+// do{
+
+// input = prompt("Введіть число");
+
+// if(input === null){
+//     console.log("Скасовано користувачем"); 
+// }
+
+
+// let inputIsNaN = parseFloat( isNaN(Number(input)))
+//  inputIsNaN ? alert("Ви ввели не число ") :  sum += Number(input);
+
+
+ 
+
+// }while(input !== null)
+
+
+// console.log("Сумма:", sum)
+// console.log("Длина:", sum.toString().length)
+
+
+
+
+
+
+
+
+
+
+
+
