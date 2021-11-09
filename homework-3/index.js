417153255fe0e1051742b1d38735ff0f1a3c9242
ilyamartynov989@gmail.com <ilyamartynov989@gmail.com>
2021-11-09 23:20:53 +0200

    third homework

---
+++ homework-3/index.js
@@ -0,0 +1,71 @@
+// --------------------------------------------------------------------- Задание 1 ---------------------------------------------------------------------------------- 
+
+// const numberArray = [0, 1, false, 2, undefined, '', 3, null];
+
+// for(let number of numberArray){
+//     if(!number){
+//         continue;
+
+//     };
+//     console.log(number);
+// }
+
+
+// --------------------------------------------------------------------- Задание 2 ---------------------------------------------------------------------------------- 
+
+
+// console.log("Задание а");
+// console.log("");
+
+// const rundomNumberArr= [0,5,25,78,95,46,24,65,81,37,154,954];
+ 
+// let sum =0;
+
+// for(let number of rundomNumberArr){
+//     sum += number;
+    
+
+// }
+
+// console.log("Сумма: " + sum);
+// console.log("Среднее значение: " + sum / rundomNumberArr.length);
+
+
+// console.log("");
+// console.log("");
+// console.log("Задание Б");
+// console.log("");
+
+
+
+// for(let number of rundomNumberArr){
+//     if(number === 25){
+//         console.log("Мы нашли его, " + "это номер " + number + ", его индекс:  " + rundomNumberArr.indexOf(number));
+// continue
+//     }
+
+//     if(number === 27){
+//         console.log("Мы нашли его, " + "это номер " + number + ", его индекс:  " + rundomNumberArr.indexOf(number));
+// continue
+
+//     }
+
+//     if(number === 65){
+//         console.log("Мы нашли его, " + "это номер " + number + ", его индекс:  " + rundomNumberArr.indexOf(number));
+// continue
+//     }
+    
+// }
+
+
+// --------------------------------------------------------------------- Задание 3 ---------------------------------------------------------------------------------- 
+
+// const rundomNumberArr = [];
+
+// for (let i = 0; i < 10; i += 1) {
+//     rundomNumberArr.push(i);
+//       console.log(Math.floor(Math.random(rundomNumberArr) * 10));
+//   }
+  
+
+
